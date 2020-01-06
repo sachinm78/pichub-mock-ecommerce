f18e20216b3fb8be0604afe941f6fba8cda88ff5
sachinm78 <sachinm78@gmail.com>
2020-01-06 10:56:22 -0600

    created Header component and imported into App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react"
+import Header from "./components/Header"
 
 function App() {    
     return (
         <div>
+            <Header />
             <h1>Home Page</h1>
         </div>
     )
