9b14dfc8ac260d49b5cab8e1e4e450b15f81b2b5
sachinm78 <sachinm78@gmail.com>
2020-01-06 12:41:44 -0600

    successfully displayed images on Photos page; added css grid styling; photos are displaying as intended!

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,9 @@
 import React from "react"
 
-function Image({className}) {
+function Image({className, img}) {
     return (
         <div className={`${className} image-container`}>
-            <img src={???} className="image-grid"/>
+            <img src={img.url} className="image-grid"/>
         </div>
     )
 }
