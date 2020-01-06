7528a583a642e244892d858c3d6446e141b09fdc
sachinm78 <sachinm78@gmail.com>
2020-01-06 11:16:12 -0600

    set up react router and updated App.js and Header component.  Links are working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Header from "./components/Header"
+import {Switch, Route} from "react-router-dom"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
 import styles from "./styles.css"
@@ -8,7 +9,15 @@ function App() {
     return (
         <div>
             <Header />
-            <h1>Home Page</h1>
+            <Switch>
+                <Route exact path="/">
+                    <Photos />
+                </Route>
+                
+                <Route path="/cart">
+                    <Cart />
+                </Route>
+            </Switch>
         </div>
     )
 }
