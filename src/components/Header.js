4e68a5c55fcef4ce8f6fe86ec88f7e10771d26dc
sachinm78 <sachinm78@gmail.com>
2020-01-08 13:25:53 -0600

    fixed typo on site name in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ function Header() {
     const cartClassName = cartItems.length > 0 ? "ri-shopping-cart-fill" : "ri-shopping-cart-line"
     return (
         <header>
-            <Link to="/"><h2>Pic Some</h2></Link>
+            <Link to="/"><h2>Pic Hub</h2></Link>
             <Link to="/cart">
                 <i className={`${cartClassName} ri-fw ri-2x`}></i>
             </Link>
